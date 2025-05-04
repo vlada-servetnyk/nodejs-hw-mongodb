ae40ed14c5c46668b91cf8c15df281a52e4dc820
Vladosha <vladaservetnik@gmail.com>
2025-05-04 18:45:34 +0300

    get all contacts

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -2,6 +2,7 @@
 export const errorHandler = (error, req, res, next) => {
     const {status = 500, message = "Server error"} = error;
     res.status(status).json({
+        status,
         message,
     });
 };
