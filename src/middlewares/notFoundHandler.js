9ec0e2a7eaa1d342049b80794bb85475293fcd17
Vladosha <vladaservetnik@gmail.com>
2025-05-04 20:15:23 +0300

    by id

--- src/middlewares/notFoundHandler.js
+++ src/middlewares/notFoundHandler.js
@@ -2,6 +2,6 @@
 export const notFoundHandler = (req, res) => {
     res.status(404).json({
         status: 404,
-		message: "Route not found",
-    })
-}
+        message: "Route not found",
+    });
+};
